057d853945ae4b22daf6db3994dd2a2c021ed953
Patrick Keroulas <patrick.keroulas@gmail.com>
2018-11-02 17:18:36 -0400

    improve network setup

--- network_setup.sh
+++ network_setup.sh
@@ -12,14 +12,20 @@ fi
 ETH=$1
 GATEWAY=$(sed -n 's/GATEWAY=\(.*\)/\1/p' /etc/sysconfig/network-scripts/ifcfg-$ETH)
 
-# access source unicast IP through the gateway
+# join multicast through the media gateway
+# MULTICAST=225.16.0.1
+# ip route add $MULTICAST dev $ETH
+
+# source unicast IP should be accessible through the gateway
 # this is necessary for the reverse path resolution
 # of the source during multicast join
-SOURCE_IP=172.30.64.160
+SOURCE_SUBNET_IP=172.30.64.160
 SOURCE_MASK=27
-MULTICAST=225.16.0.1
 ip route add $SOURCE_IP/$SOURCE_MASK via $GATEWAY dev $ETH
-ip route add $MULTICAST dev $ETH
+
+# disable reverse path filtering, not necessary if reverse path are already routed
+# sysctl -w net.ipv4.conf.all.rp_filter=0
+# sysctl -w net.ipv4.conf.$ETH.rp_filter=0
 
 # allow multicast
 if iptables -C INPUT -d 224.0.0.0/4 -j ACCEPT 2> /dev/null
@@ -28,10 +34,6 @@ then
 else
     iptables -I INPUT 1 -d 224.0.0.0/4 -j ACCEPT
 fi
-
-# disable reverse path filtering
-# sysctl -w net.ipv4.conf.all.rp_filter=0
-# sysctl -w net.ipv4.conf.$ETH.rp_filter=0
 
 # input buffer
 size=671088640
