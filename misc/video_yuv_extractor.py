95e56ab4b329aa1796499091983e1ecbb5e4a59b
Patrick Keroulas <patrick.keroulas@radio-canada.ca>
2020-05-22 17:26:20 -0400

    misc: add multiple mode yuv conversion

--- misc/video_yuv_extractor.py
+++ misc/video_yuv_extractor.py
@@ -7,10 +7,6 @@
 #
 # At CBC, pix format commonly found in RFC4175 payload is packed YUV
 # 4:2:2 10-bit/component but none of this not support by ffmpeg.
-# These conversions are possible:
-# 1) packet 4:2:2 8-bit  > FFmpeg: "uyvy422"
-# 2) planar 4:2:2 8-bit  > FFmpeg: "yuv422p"
-# 3) planar 4:2:2 10-bit > FFmpeg: "yuv422p10be" ?????????????????
 #
 # More info about YUV pixel formats:
 # http://www.fourcc.org/yuv.php#UYVY
@@ -29,13 +25,20 @@ import shutil
 from collections import namedtuple
 from bitstruct import *
 
-if (len(sys.argv) < 3):
-    print(sys.argv[0] + ' <pcap file> <dst IP filter>\n\
-    output: output.yuv (uyvy422)')
+if (len(sys.argv) < 4):
+    print(sys.argv[0] + '<pcap file> <dst IP filter> <yuv_mode>\n\
+    yuv_mode: 1 packet 4:2:2 8-bit  > FFmpeg: "uyvy42u"\n\
+              2 planar 4:2:2 8-bit  > FFmpeg: "yuv422p"\n\
+              3 planar 4:2:2 10-bit > FFmpeg: "yuv422p10be"\n\
+\n\
+    output: output.yuv')
     exit(-1)
 
 pcap = sys.argv[1]
 filter = 'dst ' + sys.argv[2]
+YUV_MODE = ['uyvy422', 'yuv422p', 'yuv422p10be']
+yuv_mode = YUV_MODE[int(sys.argv[3])-1]
+yuv_file = open('output.yuv', mode='wb')
 
 def showProgess(progress):
     sys.stdout.write("%s                                          \r" % (progress) )
@@ -65,15 +68,15 @@ def showProgess(progress):
       +---------------------------------------------------------------+
 """
 
-yuv_file = open('output.yuv', mode='wb')
 recording = False
 frame_counter = 0
 y_stream = io.BytesIO(bytes())
 u_stream = io.BytesIO(bytes())
 v_stream = io.BytesIO(bytes())
-n_bytes = 0
+
 def extractPayload(pkt):
-    global recording, yuv_file, frame_counter, y_stream, u_stream, v_stream, n_bytes
+    global recording, frame_counter
+    global yuv_mode, yuv_file, y_stream, u_stream, v_stream
     i_stream = StringIO.StringIO(pkt.load)
     buf = i_stream.getvalue()
 
@@ -102,28 +105,35 @@ def extractPayload(pkt):
         unpacked = cf.unpack(i_stream.read(pgroup_size))
         p = PGroup(*unpacked)
 
-        # uyvy422
-        #y_stream.write(pack('u8u8u8u8', p.u>>2, p.y0>>2, p.v>>2, p.y1>>2)) # less performant
-        #y_stream.write(chr(p.u>>2) + chr(p.y0>>2) + chr(p.v>>2) + chr(p.y1>>2))
+        if (yuv_mode == 'uyvy422'):
+            #y_stream.write(pack('u8u8u8u8', p.u>>2, p.y0>>2, p.v>>2, p.y1>>2)) # less performant
+            y_stream.write(chr(p.u>>2) + chr(p.y0>>2) + chr(p.v>>2) + chr(p.y1>>2))
 
-        # yuv422p
-        y_stream.write(chr(p.y0>>2) + chr(p.y1>>2))
-        u_stream.write(chr(p.u>>2))
-        v_stream.write(chr(p.v>>2))
+        elif (yuv_mode == 'yuv422p'):
+            y_stream.write(chr(p.y0>>2) + chr(p.y1>>2))
+            u_stream.write(chr(p.u>>2))
+            v_stream.write(chr(p.v>>2))
+
+        elif (yuv_mode == 'yuv422p10be'):
+            # TODO see if chr()... is faster
+            y_stream.write(pack('u16u16', p.y0, p.y1))
+            u_stream.write(pack('u16', p.u))
+            v_stream.write(pack('u16', p.v))
 
     if marker == 1:
         y_stream.seek(0)
-        u_stream.seek(0)
-        v_stream.seek(0)
         shutil.copyfileobj(y_stream, yuv_file)
-        shutil.copyfileobj(u_stream, yuv_file)
-        shutil.copyfileobj(v_stream, yuv_file)
-
         y_stream.seek(0)
-        u_stream.seek(0)
-        v_stream.seek(0)
+
+        if (not yuv_mode == 'uyvy422'):
+            u_stream.seek(0)
+            v_stream.seek(0)
+            shutil.copyfileobj(u_stream, yuv_file)
+            shutil.copyfileobj(v_stream, yuv_file)
+            u_stream.seek(0)
+            v_stream.seek(0)
+
         frame_counter += 1
-        n_bytes = 0
 
 # GO!
 print('Filter dst IP: \'' + filter + '\'')
@@ -132,4 +142,8 @@ print('Processing...')
 sniff(offline=pcap, filter=filter, store = 0, prn = extractPayload)
 yuv_file.close()
 
-print('Done.                  ')
+print('Done.                                ')
+
+# fix dimensions
+print("Suggestion:\n\
+        ffplay -f rawvideo -vcodec rawvideo -s 1920*540 -pix_fmt " + yuv_mode + " -i output.yuv")
