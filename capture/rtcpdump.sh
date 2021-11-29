588b50801829f130cf67a7dde3cb40401a58be24
Patrick Keroulas <patrick.keroulas@radio-canada.ca>
2021-11-29 15:30:40 -0500

    capture: support password file

--- capture/rtcpdump.sh
+++ capture/rtcpdump.sh
@@ -10,8 +10,9 @@
 # - login to remote through ssh
 # - detect if remote is normal linux host or Arista switch
 # - if Arista, init a monitor session that mirrors targeted port to cpu interface
-# - launch tcpdump in remote bash and output to stdout
+# - launch tcpdump in remote bash and output to stdout (raw)
 # - launch local wireshark and read from stdin
+# - clean up monitor session on wireshark exited
 #
 # - tested on Arista switches (DCS-7280CR2A-30, EOS-4.24.2.1F)
 # - capturing a high bitrate port isn't good idea given the additional
@@ -25,18 +26,21 @@ pkt_count=10000
 usage(){
     echo "Usage:
     $0 -r <user>@<remote_ip> -p <password> -i <remote_interface> [-c <packet_count>] [-v] ['filter_expression']
-notes:
-    -r can refer to connexion in your local ssh config
-    -p password used if you have sshpass installed
+
+params:
+    -r ssh path; can be an alias in your local ssh config
+    -p password used if you have sshpass installed, can be a password file
     -i remote interface name. On a switch, it can either be simple '10' or a part of a quad-port '10/2'
     -c limit of captured packets (default $pkt_count as safety for network)
     -v verbose
     filter_expression is passed to remote tcpdump
-exple:
+
+example:
     monitor PTP on Arita switch:
-    $0 -r user@$IP -p passwd -i 10/1 'dst port 319 or dst port 320'
+    $0 -r user@server -p passwd -i 10/1 'dst port 319 or dst port 320'
+
     monitor DHCP on Linux host:
-    $0 -r user@$IP -c 50 -i eth0 'port 67 or port 68'
+    $0 -r user@server -c 50 -i eth0 'port 67 or port 68'
     " >&2
 }
 
@@ -49,7 +53,11 @@ while getopts ":r:p:i:c:v:" o; do
             iface=${OPTARG}
             ;;
         p)
-            pass=${OPTARG}
+            if [ -f ${OPTARG} ]; then
+                pass=$(cat ${OPTARG} | tr -d '\n' | tr -d '\r' | tr -d '\0')
+            else
+                pass=${OPTARG}
+            fi
             ;;
         c)
             pkt_count=${OPTARG}
@@ -91,6 +99,7 @@ fi
 echo "Mode : $remote"
 
 if [ "$remote" = "arista" ]; then
+    echo "------------------------"
     echo "Create a monitor session."
     $ssh_cmd "enable
     conf
@@ -102,16 +111,19 @@ if [ "$remote" = "arista" ]; then
     sessions=$($ssh_cmd "enable
     conf
     show monitor session")
+    echo "------------------------"
     echo "$sessions"
     cpu_iface=$(echo "$sessions" | grep $session -A 10 | grep Cpu | sed 's/.*(\(.*\))/\1/')
 
     trap 'echo Interruption.' 2 # catch SIGINT (Ctrl-C) to exit Arista bash properly
 
+    echo "------------------------"
     echo "Capture on Cpu($cpu_iface) ......."
     $ssh_cmd "enable
     conf
     bash tcpdump -i $cpu_iface -c $pkt_count -U -s0 -w - $filter" | wireshark -k -i -
 
+    echo "------------------------"
     echo "Cleanup."
     $ssh_cmd "enable
     conf
