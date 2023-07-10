ca3b4aa5d3d2aa8f197788d49fa9f0ee2930c94e
Patrick Keroulas <patrick.keroulas@radio-canada.ca>
2023-07-10 16:16:59 -0400

    ebulist: update install
    Remove installation from ebu_list_ctl which before it's too large.
    Don't wget the docker-compose file because it's already part of the
    docs/ directory. Reminder: the whole repo needs to be fetch because the
    probe is one of the nodejs apps.

--- ebu-list/install.sh
+++ ebu-list/install.sh
@@ -18,9 +18,28 @@ install_list()
     usermod -a -G pcap $ST2110_USER
     usermod -a -G docker $ST2110_USER
 
-    LIST_DIR=/home/$ST2110_USER/pi-list
-    install -m 755 $TOP_DIR/ebu-list/ebu_list_ctl /usr/sbin/
-    #TODO docker not active yet
-    su $ST2110_USER -c "ebu_list_ctl install"
+    ln -fs $TOP_DIR/ebu-list/ebu_list_ctl /usr/sbin/ebu_list_ctl
+
+    # su $ST2110_USER -c "ebu_list_ctl install"
+    cd /home/$ST2110_USER
+    git clone https://github.com/ebu/pi-list.git $LIST_PATH
+    cd $LIST_PATH
+    git submodule update --init --recursive
+    ./scripts/setup_build_env.sh
+
+    # dev mode means build application from source
+    if [ $LIST_DEV = "true" ]; then
+        ./scripts/deploy/deploy.sh
+    else
+        # whereas non dev mode means install from public docker image
+        cd $LIST_PATH/docs
+        docker-compose pull
+
+        # but still need to build node apps to run the capture probe
+        cd $LIST_PATH/
+        ./scripts/build_node.sh
+    fi
+
+    chown -R $ST2110_USER:$ST2110_USER $LIST_PATH
 }
 
